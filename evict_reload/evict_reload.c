36c77fc21476749e1985ccd7af2798a031fbc796
pwade11 <pfw@wadefam.com>
2025-10-12 13:15:01 -0500

    Working on eviction set idea to test code

--- evict_reload/evict_reload.c
+++ evict_reload/evict_reload.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <x86intrin.h>
 #include <stdbool.h>
+#include candidate_set_linked_list.h
 
 #define PAGE_SIZE 4096
 #define CACHE_LINE_SIZE 64
@@ -86,8 +87,55 @@ uint64_t calibrate_latency(){
 	return foo;
 }
 
+bool check_conflict(uint8_t *target_addr, uint8_t **conflict_set, int conflict_set_size, 
+	uint8_t **l2_occupy_set, int l2_occupy_set_size, uint64_t llc_miss_threshold){
+	// TODO FIGURE OUT GETTING l2_occupy_set
+	// TODO FIGURE OUT GETTING llc_miss_threshold
 
-void generate_ev(uint8_t **EVtd, uint8_t **EVl2_mul, uint8_t *page_mul){
+	// This comes from the paper
+	_maccess(target_addr);
+	for (int i = 0; i < conflict_set_size; i++){
+		_maccess(conflict_set[i]);
+	}
+	// evict U from the L2 to the LLC
+	for (int i = 0; i < l2_occupy_set_size; i ++){
+		_maccess(l2_occupy_set[i]);
+	}
+	uint64_t start = _timer_start();
+	_maccess(target_addr);
+	uint64_t lat = _timer_end() - start;
+
+	return lat >= llc_miss_threshold;
+
+}
+
+
+uint8_t ** find_EV(uint8_t **candidate_set, int candidate_size){
+	// find eviction set function from the paper
+	// for algorithm to work, all addr in CS must have the same LLC set index bits, and CS contains more than Wslice addresses for each slice
+	// SHOULD RETURN WITH Wslice elements
+	int max_rand = candidate_size - 10;
+	int min_rand = 0;
+
+	int rand_test_addr_index = (rand() % (max_rand - min_rand + 1)) + min_rand;
+	uint8_t *test_addr = candidate_set[rand_test_addr_index];
+	uint8_t ** EV ; //TODO ALLOCATE
+	int EV_size = 0;
+
+	// TODO lines 4-14 in the paper
+
+	// this is lines 15-19 in the paper
+	for (int i = 0; i < candidate_size; i++){
+		if (check_conflict(candidate_set[i], EV, EV_size, ??, ??, ??)){
+
+		}
+	}
+
+}
+
+void generate_ev(uint8_t **EVtd, uint8_t **EVl2_mul){
+	// Peter's attempt to do it
+	// based on apendix of paper 
 	// TODO GENERATE EVICTION SETS
 
 	// TODO BE VERY CAREFUL ABOUT MEMORY HANDLING
@@ -143,6 +191,7 @@ void attacker_side_channel(uint64_t start_tsc, uint64_t msg_len, uint8_t **EVl2_
 	//main receiver loop
 	for (uint64_t i = 0; i < msg_len; i++){
 		// evict
+		// TODO THE PAPER MENTIONS ACCESSING THESE 6 TIMES, WHY?
 		busy_wait_until(next_evict);
 		next_evict += EPOCH_LENGTH;
 		for (int j = 0; j < WL2; j ++){
@@ -163,7 +212,7 @@ void attacker_side_channel(uint64_t start_tsc, uint64_t msg_len, uint8_t **EVl2_
 		// Print the results
 		uint64_t now = _rdtsc();
 		uint64_t epoch_id = (now - start_tsc) / EPOCH_LENGTH;
-		printf("EPOCH: %" PRIu64 "; Receiving: '%d' \n", epoch_id, received_val);
+		printf("EPOCH: %" PRIu64 "; Receiving: '%d', latency: %llu \n", epoch_id, received_val, lat);
 
 		// save results. Use mask to help
 		if (epoch_id < msg_len){
@@ -198,7 +247,7 @@ int main(){
 	uint64_t threshold = calibrate_latency();
 
 	// generate the eviction sets
-	generate_ev(EVtd, EVl2_mul, page_mul);
+	generate_ev(EVtd, EVl2_mul);
 
 
 	// sizeof returns number of bytes in msg, so multiply by 8 to get the total number of bits
