5426002092bf0d483a62bfc8a7f7ffd000d1c2fb
Peter F Wade <pfw275@mario.ece.utexas.edu>
2025-10-19 13:03:44 -0500

    Fixing plotting code

--- benchmark_cache/plot_benchmark_data.py
+++ benchmark_cache/plot_benchmark_data.py
@@ -9,8 +9,8 @@ with open(data_file, "r") as f:
 	for line in f:
 		if line:
 			split_data = line.strip().split(",")
-			data_N.append(split_data[0])
-			data_time.append(split_data[1])
+			data_N.append(int(split_data[0]))
+			data_time.append(int(split_data[1]))
 
 	
 plt.scatter(data_N, data_time)
